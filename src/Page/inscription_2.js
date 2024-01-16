2bc9b56e82d935964fb4fa485e7f29e0a9339733
LyneF <Lyne.Friederich@hotmail.com>
2024-01-16 08:43:14 +0100

    :sparkles: Ajout form Hock sur inscription étape 1, 2, 3.

--- src/Page/inscription_2.js
+++ src/Page/inscription_2.js
@@ -2,8 +2,13 @@ import React from 'react';
 import Header from "../Component/header";
 import Footer from "../Component/footer";
 import { useTranslation } from 'react-i18next';
+import { useForm} from "react-hook-form";
 function Inscription_2() {
     const { t } = useTranslation();
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <>
             <Header/>
@@ -12,25 +17,30 @@ function Inscription_2() {
                     <div className="text-5xl font-bold text-center text-primary mt-8">{t('Activites')}</div>
                     <div className="text-2xl font-bold text-center text-primary mt-8">{t('ApprendrePlusEntreprise')}</div>
                     <div className="card w-96 mx-auto border-2 border-primary my-6">
-                        <div className="card-body items-center text-center">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body items-center text-center">
                             <div className="grid grid-cols-2 gap-4">
                                 <label className="form-control w-full max-w-xs">
                                     <div className="label">
                                         <span className="label-text">{t('Nom')}</span>
                                     </div>
-                                    <input type="text" placeholder="Pomme & Cie"
-                                           className="input input-bordered w-full max-w-xs"/>
+                                    <input type="text" className="input input-bordered w-full max-w-xs" placeholder="Pomme & Cie"
+                                           {...register("name", {required: true})}/>
+                                    <div className="label">
+                                        {errors.name &&
+                                            <span className="label-text-alt text-error">Ce champ est obligatoire</span>}
+                                    </div>
                                 </label>
+                                {/*se renseigner hook pour select*/}
                                 <label className="form-control w-full max-w-xs w-auto">
                                     <div className="label">
                                         <span className="label-text">{t('Secteur')}</span>
                                     </div>
-                                    <select className="select select-bordered">
-                                        <option>Alimentaire</option>
-                                        <option>Hôtellerie - Restauration</option>
-                                        <option>Numérique</option>
-                                        <option>Arts, Culture</option>
-                                        <option>Ameublement</option>
+                                    <select {...register("sector", { required: 'Please select a sector' })} className="select select-bordered">
+                                        <option value="alimentaire">Alimentaire</option>
+                                        <option value="hotellerie">Hôtellerie</option>
+                                        <option value="numérique">Numérique</option>
+                                        <option value="art">Art</option>
+                                        <option value="ameublement">Ameublement</option>
                                     </select>
                                 </label>
                             </div>
@@ -38,13 +48,18 @@ function Inscription_2() {
                                 <div className="label">
                                     <span className="label-text">{t('SIRET')}</span>
                                 </div>
-                                <input type="text" placeholder="552 178 639 00132"
+                                <input type="text" {...register("siret", {required: true})}
+                                       placeholder="552 178 639 00132"
                                        className="input input-bordered w-full max-w-xs"/>
+                                <div className="label">
+                                    {errors.siret &&
+                                        <span className="label-text-alt text-error">Ce champ est obligatoire</span>}
+                                </div>
                             </label>
                             <div className="card-actions justify-end mt-4">
-                                <button className="btn btn-primary">{t('Suivant')}</button>
+                                <input className="btn btn-primary" type="submit" value={t('Suivant')}/>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
